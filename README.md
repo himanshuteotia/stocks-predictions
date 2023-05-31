60706f57de8c117deb37f0f3405b8e525cf8f438
NLP <74973310+nlptechguy@users.noreply.github.com>
2023-05-31 23:23:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+It includes Typescript, EsLint, Jest, Winston, Prettier and many more please check package.json.
+
+Following commands you have to run
+
+1. `npm install` && `npm start`
+
+
+2. (optional) To update packaes to latest run
+`npx npm-check-updates -u` & `npm install`
+
+3. (optional) To check any outdated npm please run `npm outdated` if you found any outdated module please repeat step 2.
---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import compression from 'compression'; // compresses requests
+import lusca from 'lusca';
+import path from 'path';
+import logger from './utils/logger.util';
+import { RoutesV1 } from './routes/v1/v1.route';
+import rateLimitRequestHandler from './middlewares/rate-limit.middleware';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+export default class App {
+  constructor(public port: number) {}
+
+  createApp(): express.Application {
+    const app: express.Application = express();
+    app.use(compression());
+    app.use(lusca.xframe('SAMEORIGIN'));
+    app.use(lusca.xssProtection(true));
+    app.use(
+      express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 })
+    );
+    app.use(rateLimitRequestHandler());
+    app.use(
+      '/v1/',
+      new LoggerMiddleware().endpointsLogs,
+      new RoutesV1().router
+    );
+    return app;
+  }
+
+  startServer(): void {
+    const app = this.createApp();
+    app.listen(this.port);
+    logger.info(`server is listening on ${this.port}`);
+  }
+}
